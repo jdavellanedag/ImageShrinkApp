09dbed65538dda1e263f7b484beca9cae5b46ee2
jdavellanedag <juldave.gal@hotmail.com>
2020-06-22 23:35:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## Simple Study ImageShrink App
+
+This is a electron app for image shrinking
+
+### Install
+
+#### Install dependencies
+
+```
+npm install
+```
+
+or
+
+```
+yarn
+```
+
+### Usage
+
+#### Run the app
+
+```
+npm run start
+```
+
+or
+
+```
+yarn start
+```
+
+#### Build the app (automatic)
+
+```
+npm run package
+```
+
+or
+
+```
+yarn package
+```
+
+#### Build the app (manual)
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
---
+++ main.js
@@ -0,0 +1,112 @@
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
+
+// Set env - development/production
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+const isWin = process.platform === "win32" ? true : false;
+
+let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    backgroundColor: "white",
+  });
+
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: "About ImageShrink",
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: "white",
+  });
+  aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
+app.on("ready", () => {
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on("closed", () => (mainWindow = null));
+  /*   globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () =>
+    mainWindow.toggleDevTools()
+  ); */
+});
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(isWin
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+  /*{     label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        accelerator: "CmdOrCtrl+W",
+        click: () => app.quit(),
+      },
+    ],}, */
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
